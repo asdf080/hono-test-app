5b9b40a316543b2768e2a8aa35e2485d6c9c60ba
dahee <ai04120@naver.com>
2025-09-02 09:37:52 +0900

    feat: tanstack query 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { QueryProvider } from "@/lib/provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
@@ -23,7 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <QueryProvider>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      </QueryProvider>
     </html>
   );
 }
