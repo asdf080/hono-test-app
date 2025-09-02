8ddb2786b776c42d210d5f850c20767cab63550e
dahee <ai04120@naver.com>
2025-09-02 10:39:34 +0900

    feat: Swagger UI 미들웨어 추가

--- src/app/api/[...route]/route.ts
+++ src/app/api/[...route]/route.ts
@@ -2,10 +2,13 @@ import { logger } from "@/lib/hono/middleware/logger";
 import todosRoute from "@/lib/hono/routes/todos";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { setupSwagger } from "@/lib/hono/middleware/swagger";
 
 const app = new Hono().basePath("/api");
 
 app.use("*", logger);
+setupSwagger(app);
+
 const routes = app.route("/todos", todosRoute);
 
 const handler = handle(app);
